a3b32e35b497d40f3f23533559879d15d86c15cf
Willem Wigman <ww@integer-net.de>
2020-06-23 12:32:15 +0200

    initial commit

---
+++ src/ViewModel/ProductAttributes.php
@@ -0,0 +1,134 @@
+<?php
+declare(strict_types=1);
+
+namespace Wigman\Tailwind\ViewModel;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\AbstractAttribute;
+use Magento\Framework\Phrase;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class ProductAttributes implements ArgumentInterface
+{
+    /**
+     * @var Product
+     */
+    protected $_product = null;
+
+    /**
+     * @var Registry
+     */
+    protected $coreRegistry = null;
+
+    /**
+     * @var PriceCurrencyInterface
+     */
+    private $priceCurrency;
+
+    /**
+     * @param Registry $registry
+     * @param PriceCurrencyInterface $priceCurrency
+     */
+    public function __construct(
+        Registry $registry,
+        PriceCurrencyInterface $priceCurrency
+    ) {
+        $this->coreRegistry = $registry;
+        $this->priceCurrency = $priceCurrency;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        if (!$this->_product) {
+            $this->_product = $this->coreRegistry->registry('product');
+        }
+        return $this->_product;
+    }
+
+    public function getAttributeFromLayoutConfig($config)
+    {
+        $product = $this->getProduct();
+        $code = $config['code'];
+        $attribute = $product->getResource()->getAttribute($code);
+
+        if (!$attribute) { return; }
+
+        $call = $config['call'] ?: 'default';
+        $label = $config['label'] ?: 'default';
+        $cssClass = $config['css_class'] ?: 'attribute';
+
+        $defaultData = $this->getAttributeData($attribute,$product);
+
+        return [
+            'label' => ($label === 'default') ? $defaultData['label'] : $label,
+            'value' => ($call !== 'default') ? $product->{$call}() : $defaultData['value'],
+            'code' => $code,
+            'css_class' => $cssClass
+        ];
+    }
+
+    /**
+     * $excludeAttr is optional array of attribute codes to exclude them from additional data array
+     *
+     * @param array $excludeAttr
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getAllVisibleAttributes(array $excludeAttr = [])
+    {
+        $data = [];
+        $product = $this->getProduct();
+        $attributes = $product->getAttributes();
+        foreach ($attributes as $attribute) {
+            if ($this->isVisibleOnFrontend($attribute, $excludeAttr)) {
+                $attributeData = $this->getAttributeData($attribute,$product);
+                if ($attributeData && $attributeData['value'])
+                {
+                    $data[$attribute->getAttributeCode()] = $attributeData;
+                }
+            }
+        }
+        return $data;
+    }
+
+    public function getAttributeData($attribute, $product)
+    {
+        $value = $attribute->getFrontend()->getValue($product);
+
+        if ($value instanceof Phrase) {
+            $value = (string)$value;
+        } elseif ($attribute->getFrontendInput() == 'price' && is_string($value)) {
+            $value = $this->priceCurrency->convertAndFormat($value);
+        }
+
+        return [
+            'label' => $attribute->getStoreLabel(),
+            'value' => $value,
+            'code' => $attribute->getAttributeCode(),
+        ];
+
+    }
+
+    /**
+     * Determine if we should display the attribute on the front-end
+     *
+     * @param AbstractAttribute $attribute
+     * @param array $excludeAttr
+     * @return bool
+     * @since 103.0.0
+     */
+    protected function isVisibleOnFrontend(
+        AbstractAttribute $attribute,
+        array $excludeAttr
+    ) {
+        return (
+            $attribute->getIsVisibleOnFront()
+            && !in_array($attribute->getAttributeCode(), $excludeAttr)
+        );
+    }
+}
---
+++ src/ViewModel/ProductPage.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace Wigman\Tailwind\ViewModel;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\AbstractAttribute;
+use Magento\Framework\Phrase;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class ProductPage implements ArgumentInterface
+{
+    /**
+     * @var Product
+     */
+    protected $_product = null;
+
+    /**
+     * @var Registry
+     */
+    protected $coreRegistry = null;
+
+    /**
+     * @var PriceCurrencyInterface
+     */
+    private $priceCurrency;
+
+    /**
+     * @param Registry $registry
+     * @param PriceCurrencyInterface $priceCurrency
+     */
+    public function __construct(
+        Registry $registry,
+        PriceCurrencyInterface $priceCurrency
+    ) {
+        $this->coreRegistry = $registry;
+        $this->priceCurrency = $priceCurrency;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        if (!$this->_product) {
+            $this->_product = $this->coreRegistry->registry('product');
+        }
+        return $this->_product;
+    }
+
+    public function getShortDescription(): string
+    {
+        $product = $this->getProduct();
+
+        if ($shortDescription = $product->getShortDescription()) {
+            return $shortDescription;
+        }
+
+        if ($description = $product->getDescription()) {
+            return $this->excerptFromDescription($description);
+        }
+        return "";
+    }
+
+    protected function excerptFromDescription(string $description): string
+    {
+        // if we have at least one <p></p>, take the first one as excerpt
+        if ( $paragraphs = preg_split('#</p><p>|<p>|</p>#i', $description, -1, PREG_SPLIT_NO_EMPTY)) {
+            return strip_tags($paragraphs[0]);
+        }
+        // otherwise, take the first sentence
+        return explode('.', strip_tags($description))[0] . '.';
+    }
+}
