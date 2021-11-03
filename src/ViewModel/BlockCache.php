16619172199226a5d46b8297b75b9f37a66f30da
Vinai Kopp <vinai@netzarbeiter.com>
2021-11-03 17:25:26 +0100

    Add methods to retrieve cache tags
    Closes issue #95

--- src/ViewModel/BlockCache.php
+++ src/ViewModel/BlockCache.php
@@ -10,8 +10,14 @@ declare(strict_types=1);
 
 namespace Hyva\Theme\ViewModel;
 
+use Magento\Framework\DataObject\IdentityInterface;
 use Magento\Framework\Serialize\Serializer\Json as JsonSerializer;
+use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+use function array_map as map;
+use function array_merge as merge;
+use function array_unique as unique;
 
 class BlockCache implements ArgumentInterface
 {
@@ -31,5 +37,28 @@ class BlockCache implements ArgumentInterface
     public function hashCacheKeyInfo(array $cacheInfo): string
     {
         return sha1($this->jsonSerializer->serialize($cacheInfo));
+    }
+
+    /**
+     * @param AbstractBlock $block
+     * @return string[]
+     */
+    public function getCacheTagsFromBlock(AbstractBlock $block): array
+    {
+        $tags = $block->getData('cache_tags') ?? [];
+        return $block instanceof IdentityInterface
+            ? unique(merge($tags, $block->getIdentities() ?: []))
+            : $tags;
+    }
+
+    /**
+     * @param AbstractBlock $block
+     * @return string[]
+     */
+    public function collectBlockChildrenCacheTags(AbstractBlock $block): array
+    {
+        $childBlocks = map([$block->getLayout(), 'getBlock'], $block->getChildNames());
+        return merge([], ...map([$this, 'getCacheTagsFromBlock'], $childBlocks));
+
     }
 }
