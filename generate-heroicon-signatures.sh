71d5a483d6e82434cd518a862d08882648b287c2
Vinai Kopp <vinai@netzarbeiter.com>
2022-01-24 14:28:38 +0100

    Adjust method annotations to match SvgIcons::renderHtml
    See \Hyva\Theme\ViewModel\SvgIcons::renderHtml
    
    Closes #128

--- generate-heroicon-signatures.sh
+++ generate-heroicon-signatures.sh
@@ -3,5 +3,5 @@ for FILE in src/view/frontend/web/svg/heroicons/outline/*.svg
 do
     echo -n " * @method string "
     echo -n $(basename -s .svg $FILE | sed -E 's/-(.)/\U\1/g')
-    echo "Html(string \$classnames = '', ?int \$width = null, ?int \$height = null)"
+    echo "Html(string \$classnames = '', ?int \$width = 24, ?int \$height = 24, array \$attributes = [])"
 done
