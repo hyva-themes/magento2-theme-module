009167acf9bab7b81293823da003dfae6688d825
Vinai Kopp <vinai@netzarbeiter.com>
2022-06-09 15:49:17 +0200

    #178 Add selected swatch to list item cache key if swatches are enabled

--- src/ViewModel/SwatchRenderer.php
+++ src/ViewModel/SwatchRenderer.php
@@ -12,10 +12,14 @@ namespace Hyva\Theme\ViewModel;
 
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\ResourceModel\Eav\Attribute;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Eav\Model\Config as EavConfig;
 use Magento\Framework\App\Request\Http as HttpRequest;
+use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Swatches\Helper\Data as SwatchHelper;
+use function array_map as map;
+use function array_keys as keys;
 
 class SwatchRenderer implements ArgumentInterface
 {
@@ -35,13 +39,23 @@ class SwatchRenderer implements ArgumentInterface
     private $eavConfig;
 
     /**
+     * @var BlockCache
+     */
+    private $blockCache;
+
+    /**
      * @param SwatchHelper $swatchHelper
      */
-    public function __construct(SwatchHelper $swatchHelper, HttpRequest $httpRequest, EavConfig $eavConfig)
-    {
+    public function __construct(
+        SwatchHelper $swatchHelper,
+        HttpRequest $httpRequest,
+        EavConfig $eavConfig,
+        BlockCache $blockCache
+    ) {
         $this->swatchHelper = $swatchHelper;
         $this->httpRequest  = $httpRequest;
         $this->eavConfig    = $eavConfig;
+        $this->blockCache   = $blockCache;
     }
 
     /**
@@ -52,17 +66,31 @@ class SwatchRenderer implements ArgumentInterface
         return $this->swatchHelper->isSwatchAttribute($attribute);
     }
 
+    public function beforeListItemToHtml(AbstractBlock $itemRendererBlock, Product $product): void
+    {
+        if ($product->getTypeId() !== Configurable::TYPE_CODE) {
+            return;
+        }
+
+        $filterAttributes = $this->getUsedSwatchFilters($product);
+        ksort($filterAttributes);
+        $swatchCacheKey = implode('', map(function (string $code) use ($filterAttributes): string {
+            return "$code={$filterAttributes[$code]}";
+        }, keys($filterAttributes)));
+
+        if ($swatchCacheKey) {
+            $newKey = $this->blockCache->hashCacheKeyInfo([$itemRendererBlock->getData('cache_key'), $swatchCacheKey]);
+            $itemRendererBlock->setData('cache_key', $newKey);
+        }
+    }
+
     /**
      * @see \Magento\Swatches\Model\Plugin\ProductImage::getFilterArray
      */
-    public function getUsedSwatchFilters(Product $product): array
+    private function getUsedSwatchFilters(Product $product): array
     {
-        if ($product->getTypeId() === Product\Type::TYPE_SIMPLE) {
-            return [];
-        }
-
-        $requestParams        = $this->httpRequest->getParams();
-        $allAttributes        = $this->eavConfig->getEntityAttributes(Product::ENTITY, $product);
+        $requestParams = $this->httpRequest->getParams();
+        $allAttributes = $this->eavConfig->getEntityAttributes(Product::ENTITY, $product);
         $usedFilterAttributes = [];
         foreach ($requestParams as $code => $value) {
             if (isset($allAttributes[$code])) {
