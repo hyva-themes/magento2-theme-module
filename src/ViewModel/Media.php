43b1f3a9672510cca83c847fe48f5b4877944be8
pykettk <pykettk@gmail.com>
2022-06-28 13:22:53 +0100

    Create Media View Model
    + Add function get get media URL

---
+++ src/ViewModel/Media.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Hyvä Themes - https://hyva.io
+ * Copyright © Hyvä Themes 2020-present. All rights reserved.
+ * This product is licensed per Magento install
+ * See https://hyva.io/license
+ */
+declare(strict_types=1);
+
+namespace Hyva\Theme\ViewModel;
+
+use Magento\Framework\Exception\NosuchEntityException;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class Media implements ArgumentInterface
+{
+    /** @var StoreManagerInterface */
+    private StoreManagerInterface $storeManager;
+
+    /**
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        StoreManagerInterface $storeManager
+    ) {
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMediaUrl(): string
+    {
+        try {
+            return $this->storeManager->getStore()->getBaseUrl(UrlInterface::URL_TYPE_MEDIA);
+        } catch (NoSuchEntityException $e) {
+            return '';
+        }
+    }
+}
