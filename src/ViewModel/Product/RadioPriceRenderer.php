8ef099026793d4b36d29cd5b6964b9af949c086c
Vinai Kopp <vinai@netzarbeiter.com>
2023-06-20 11:40:05 +0200

    #626 Add type annotation for parameter and missing license header

--- src/ViewModel/Product/RadioPriceRenderer.php
+++ src/ViewModel/Product/RadioPriceRenderer.php
@@ -1,10 +1,18 @@
 <?php
+/**
+ * Hyvä Themes - https://hyva.io
+ * Copyright © Hyvä Themes 2020-present. All rights reserved.
+ * This product is licensed per Magento install
+ * See https://hyva.io/license
+ */
 
 declare(strict_types=1);
 
 namespace Hyva\Theme\ViewModel\Product;
 
 use Magento\Bundle\Block\Catalog\Product\View\Type\Bundle\Option;
+use Magento\Bundle\Model\Selection;
+use Magento\Catalog\Model\Product;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class RadioPriceRenderer implements ArgumentInterface
@@ -18,7 +26,7 @@ class RadioPriceRenderer implements ArgumentInterface
     }
 
     /**
-     * @param $selection
+     * @param Product|Selection $selection
      * @return string
      */
     public function getSelectionTitlePrice($selection): string
