9c09816347df74c9eee9b444893708b63ec15fbe
grimlink <sean.grimlink@gmail.com>
2023-11-13 14:35:30 +0100

    IMP: move customer helper to customer vieemodal
    Also updated code from feedback

--- src/ViewModel/Customer.php
+++ src/ViewModel/Customer.php
@@ -5,11 +5,13 @@
  * This product is licensed per Magento install
  * See https://hyva.io/license
  */
- 
+
 declare(strict_types=1);
 
 namespace Hyva\Theme\ViewModel;
 
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\App\Http\Context as HttpContext;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
@@ -21,11 +23,15 @@ class Customer implements ArgumentInterface
     protected $httpContext;
 
     /**
-     * @param HttpContext $httpContext
+     * @var CustomerRepositoryInterface
      */
+    protected $customerRepositoryInterface;
+
     public function __construct(
+        CustomerRepositoryInterface $customerRepositoryInterface,
         HttpContext $httpContext
     ) {
+        $this->customerRepositoryInterface = $customerRepositoryInterface;
         $this->httpContext = $httpContext;
     }
 
@@ -37,5 +43,21 @@ class Customer implements ArgumentInterface
     public function customerLoggedIn()
     {
         return (bool)$this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
+    }
+
+    /**
+     * Get Customer by Customer ID
+     *
+     * @return CustomerInterface
+     */
+    private function getCustomerById(int $id)
+    {
+        return $this->customerRepositoryInterface->getById($id);
+    }
+
+    public function getEmailById(int $id): string
+    {
+        $customer = $this->getCustomerById($id);
+        return $customer ? $customer->getEmail() : '';
     }
 }
