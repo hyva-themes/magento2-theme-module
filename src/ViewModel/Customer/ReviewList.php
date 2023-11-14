395c22da33c9f40932e3e89e5075901cde589f2d
grimlink <sean.grimlink@gmail.com>
2023-11-14 15:44:55 +0100

    IMP: add collection helper

--- src/ViewModel/Customer/ReviewList.php
+++ src/ViewModel/Customer/ReviewList.php
@@ -10,10 +10,51 @@ declare(strict_types=1);
 
 namespace Hyva\Theme\ViewModel\Customer;
 
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class ReviewList implements ArgumentInterface
 {
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    protected $customerRepositoryInterface;
+
+
+    public function __construct(
+        CustomerRepositoryInterface $customerRepositoryInterface
+    ) {
+        $this->customerRepositoryInterface = $customerRepositoryInterface;
+    }
+
+    public function getCustomerReviewEmailCollection(array $items): array
+    {
+        $customerEmails = [];
+
+        foreach ($items as $review) {
+            $customerId = (int) $review->getCustomerId();
+            if (!$customerId) {
+                continue;
+            }
+
+            $customerEmails[$customerId] = $this->getEmailById($customerId);
+        }
+
+        return $customerEmails;
+    }
+
+    private function getCustomerById(int $id): CustomerInterface
+    {
+        return $this->customerRepositoryInterface->getById($id);
+    }
+
+    public function getEmailById(int $id): string
+    {
+        $customer = $this->getCustomerById($id);
+        return $customer ? $customer->getEmail() : '';
+    }
+
     /**
      * @return string
      * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
