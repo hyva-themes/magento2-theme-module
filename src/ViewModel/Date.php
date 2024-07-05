cdad02ab90590a74055b0546587600ecafc19c37
grimlink <sean.grimlink@gmail.com>
2024-07-05 10:58:42 +0200

    FIX: lint error

--- src/ViewModel/Date.php
+++ src/ViewModel/Date.php
@@ -14,6 +14,7 @@ use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class Date implements ArgumentInterface
 {
+    // phpcs:ignore Magento2.CodeAnalysis.EmptyBlock.DetectedFunction
     public function __construct()
     {
     }
