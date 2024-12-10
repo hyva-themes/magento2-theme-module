8ebf45b9d89a46db0a6bfbce9ebc1921575341ec
Vinai Kopp <vinai@netzarbeiter.com>
2024-12-10 16:42:57 +0100

    #405 Inject InlineScript view model as template variable

--- src/Plugin/TemplateEngine/PhpPlugin.php
+++ src/Plugin/TemplateEngine/PhpPlugin.php
@@ -13,6 +13,8 @@ namespace Hyva\Theme\Plugin\TemplateEngine;
 use Hyva\Theme\Model\LocaleFormatterFactory;
 use Hyva\Theme\Model\ViewModelRegistry;
 use Hyva\Theme\Service\CurrentTheme;
+use Hyva\Theme\ViewModel\InlineScript;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\ProductMetadata;
 use Magento\Framework\Locale\LocaleFormatter as MagentoLocaleFormatter;
 use Magento\Framework\View\Element\BlockInterface;
@@ -44,16 +46,23 @@ class PhpPlugin
      */
     private $currentTheme;
 
+    /**
+     * @var InlineScript|null
+     */
+    private $inlineScript;
+
     public function __construct(
         ViewModelRegistry $viewModelRegistry,
         ProductMetadata $productMetadata,
         LocaleFormatterFactory $hyvaLocaleFormatterFactory,
-        CurrentTheme $currentTheme
+        CurrentTheme $currentTheme,
+        InlineScript $inlineScript = null
     ) {
         $this->viewModelRegistry = $viewModelRegistry;
         $this->productMetadata = $productMetadata;
         $this->hyvaLocaleFormatterFactory = $hyvaLocaleFormatterFactory;
         $this->currentTheme = $currentTheme;
+        $this->inlineScript = $inlineScript ?? ObjectManager::getInstance()->get(InlineScript::class);
     }
 
     /**
@@ -93,6 +102,8 @@ class PhpPlugin
         if (!class_exists(MagentoLocaleFormatter::class) || version_compare($this->productMetadata->getVersion(), '2.4.5', '<')) {
             $dictionary['localeFormatter'] = $this->hyvaLocaleFormatterFactory->create();
         }
+
+        $dictionary['inlineScript'] = $this->inlineScript;
 
         return $dictionary;
     }
