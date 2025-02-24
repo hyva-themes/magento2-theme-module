350110134752c56cc09314711044d42f4bd172b3
Vinai Kopp <vinai@netzarbeiter.com>
2025-02-24 14:11:44 +0100

    #44 Keep getShowJs method on ModalBuilder backward compatible

--- src/Model/Modal/ModalBuilder.php
+++ src/Model/Modal/ModalBuilder.php
@@ -274,7 +274,8 @@ class ModalBuilder implements ModalBuilderInterface, ModalInterface
 
     public function getShowJs(?string $focusAfterHide = null): string
     {
-        return sprintf("this.show('%s', %s)", $this->getDialogRefName(), $focusAfterHide ? "'" . $this->escaper->escapeJs($focusAfterHide) . "'" : 'this.$event');
+        $focusAfterHideJs = $focusAfterHide ? "'" . $this->escaper->escapeJs($focusAfterHide) . "'" : 'this.$event || $event';
+        return sprintf("show('%s', %s)", $this->getDialogRefName(), $focusAfterHideJs);
     }
 
     // --- modal interface methods ---
