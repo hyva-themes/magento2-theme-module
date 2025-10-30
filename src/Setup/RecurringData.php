a86a4661791eb12c30e03000f517c561ce51b3ef
Vinai Kopp <vinai@netzarbeiter.com>
2025-10-30 18:47:58 +0100

    #483 Only set to null after upgrade to allow manual migrations of older theme versions

--- src/Setup/RecurringData.php
+++ src/Setup/RecurringData.php
@@ -18,35 +18,24 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 class RecurringData implements InstallDataInterface
 {
     /**
-     * Unset or set the parent theme id for the Hyvä default themes depending on the installed versions.
+     * Unset or set the parent theme id for the Hyvä default themes after upgrading to 1.4.0 or newer
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
         foreach ([
-            'Hyva/default'     => 'hyva-themes/magento2-default-theme',
-            'Hyva/default-csp' => 'hyva-themes/magento2-default-theme-csp',
-        ] as $themeCode => $packageName) {
-            if (Composer::isInstalled($packageName)) {
-                $this->ensureThemeHasTheCorrectParentId($setup, $themeCode, $packageName);
+                     'Hyva/default'     => 'hyva-themes/magento2-default-theme',
+                     'Hyva/default-csp' => 'hyva-themes/magento2-default-theme-csp',
+                 ] as $themeCode => $packageName) {
+            if (Composer::isInstalled($packageName) && version_compare('1.4.0', Composer::getVersion($packageName), '>=')) {
+                // Ensure the default theme parent_id is NULL for default themes >= 1.4.0
+                $this->ensureThemeParentIdIsNull($setup, $themeCode);
             }
+            // Do not set the parent theme ID to Hyva/reset since we want to allow manual migrations to the base layout reset
         }
         $setup->endSetup();
     }
 
-    private function ensureThemeHasTheCorrectParentId(ModuleDataSetupInterface $setup, string $themeCode, string $packageName): void
-    {
-        $themeVersion = Composer::getVersion($packageName);
-        if (version_compare('1.3.17.0', $themeVersion, '<')) {
-            $this->ensureThemeParentIdIsNull($setup, $themeCode);
-        } else {
-            $this->ensureParentIdIsSetToHyvaReset($setup, $themeCode);
-        }
-    }
-
-    /**
-     * Ensure the default theme parent_id is NULL for default themes >= 1.3.18
-     */
     private function ensureThemeParentIdIsNull(ModuleDataSetupInterface $setup, string $themeCode): void
     {
         $parentId = $this->getParentId($setup, $themeCode);
@@ -60,26 +49,6 @@ class RecurringData implements InstallDataInterface
     {
         $adapter = $setup->getConnection();
         $select = sprintf('SELECT parent_id FROM %s WHERE code = :code', $adapter->getTableName('theme'));
-
-        return $adapter->fetchOne($select, ['code' => $themeCode]);
-    }
-
-    /**
-     * Ensure parent_id is set to Hyva/reset id for default themes < 1.3.18
-     */
-    private function ensureParentIdIsSetToHyvaReset(ModuleDataSetupInterface $setup, string $themeCode): void
-    {
-        $resetThemeId = $this->getThemeId($setup, 'Hyva/reset');
-        if ($resetThemeId) {
-            $adapter = $setup->getConnection();
-            $adapter->update($adapter->getTableName('theme'), ['parent_id' => $resetThemeId], $adapter->quoteInto('code = ?', $themeCode));
-        }
-    }
-
-    private function getThemeId(ModuleDataSetupInterface $setup, string $themeCode)
-    {
-        $adapter = $setup->getConnection();
-        $select = sprintf('SELECT theme_id FROM %s WHERE code = :code', $adapter->getTableName('theme'));
 
         return $adapter->fetchOne($select, ['code' => $themeCode]);
     }
